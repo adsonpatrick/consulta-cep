81a58b67d7cb4ed9683534b8a8dd7bed7df38394
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2022-02-04 13:20:55 -0300

    Add files via upload

---
+++ APP/app.js
@@ -0,0 +1,37 @@
+const cep = document.querySelector("#cep");
+
+const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default'
+}
+
+const ShowData = (result) =>{
+    for (const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo];
+        }
+
+        
+    }
+}
+
+cep.addEventListener("blur", (e) => {
+    let search = cep.value.replace("-", "");
+    console.log(search);
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then(response => {
+        response.json()
+        .then(data =>{
+            console.log(data)
+            ShowData(data)
+        })
+    })
+
+    
+
+    .catch(e => {
+        console.log("ERRO: "+e)
+    })
+})
